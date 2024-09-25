92aa24a81208334f8ec8d920efd22a79370f5d53
ChornyiPavlo <pavlo.chornyi.v@gmail.com>
2024-09-25 17:19:18 +0200

    1

---
+++ js/main.js
@@ -0,0 +1,36 @@
+$(function() {
+    
+    "use strict";
+
+    //===== Section Menu Active
+
+    var scrollLink = $('.page-scroll');
+    // Active link switching
+    $(window).scroll(function () {
+        var scrollbarLocation = $(this).scrollTop();
+
+        scrollLink.each(function () {
+
+            var sectionOffset = $(this.hash).offset().top - 73;
+
+            if (sectionOffset <= scrollbarLocation) {
+                $(this).parent().addClass('active');
+                $(this).parent().siblings().removeClass('active');
+            }
+        });
+    });
+    
+    //===== close navbar-collapse when a  clicked
+
+    $(".navbar-nav a").on('click', function () {
+        $(".navbar-collapse").removeClass("show");
+    });
+
+    $(".navbar-toggler").on('click', function () {
+        $(this).toggleClass("active");
+    });
+
+    $(".navbar-nav a").on('click', function () {
+        $(".navbar-toggler").removeClass('active');
+    });
+});
